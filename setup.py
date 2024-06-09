391c2f4ca92d70ade3175a311d7f00116566268f
Evan Porter <evanwporter@gmail.com>
2024-06-09 12:57:18 -0500

    Changed setup

--- setup.py
+++ setup.py
@@ -1,27 +1,50 @@
-from setuptools import setup, Extension, find_packages
-from Cython.Build import cythonize
-import numpy as np
+from setuptools import setup, find_packages, Extension
+import os
+
+def get_extensions():
+    return [
+        Extension("Sloth.frame", ["Sloth/frame.c"]),
+        Extension("Sloth.indexer", ["Sloth/indexer.c"]),
+        Extension("Sloth.resample", ["Sloth/resample.c"]),
+        Extension("Sloth.rolling", ["Sloth/rolling.c"]),
+        Extension("Sloth.util", ["Sloth/util.c"]),
+    ]
+
+# https://github.com/FedericoStra/cython-package-example/blob/master/setup.py
+with open("requirements.txt") as fp:
+    install_requires = fp.read().strip().split("\n")
+
+with open("requirements-dev.txt") as fp:
+    dev_requires = fp.read().strip().split("\n")
 
 setup(
-    name='Sloth',
-    install_requires=[
-        "numpy",
-        "cython"
-    ],
-    ext_modules=cythonize(
-        ["Sloth/*.pyx"],
-        compiler_directives={'language_level': "3"}
-    ),
-    include_dirs=[np.get_include()],
-    packages=['Sloth'],
-    zip_safe=False,
+    name="Sloth",
+    version="1.0.0-beta.1",
+    description="Sloth module",
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
     author="Evan Porter",
+    author_email="evanwporter@gmail.com",
     url="https://github.com/evanwporter/Sloth",
-    long_description=open("README.md").read(),
-    long_description_content_type="text/markdown",
+    packages=['Sloth'],
+    ext_modules=get_extensions(),
+    include_package_data=True,
+    install_requires=install_requires,
+    extras_require={
+        "dev": dev_requires,
+        "docs": ["sphinx", "furo"]
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    zip_safe=False,
 
 )
 
 """
-python setup.py build_ext --inplace
+python setup.py sdist bdist_wheel
+pip install .
 """
